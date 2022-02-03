b3670e34818fbbeb97d4d5d46d56247a4e3e7472
akis <akis@ecoc.io>
2022-02-02 23:42:03 -0500

    python script for retrieving the key

---
+++ Planet-Earth/scripts/getKey.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+
+def main():
+    if (len(sys.argv)!=3):
+        print('Correct syntax is getKey <message> <output>')
+        return
+
+    msg=sys.argv[1]
+    out=sys.argv[2]
+
+    key=''    
+    for i in range(len(msg)):
+        op1=ord(msg[i])
+        op2=int(out[2*i:2*i+2], 16)
+        result=chr((op1^op2))
+        key=key+result
+        
+    print(key)
+    return
+
+main()
